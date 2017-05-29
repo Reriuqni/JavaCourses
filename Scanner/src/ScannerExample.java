7a3b0dc8833e5eedad53a56f627594173331b297
nixon <nixon.white@gmail.com>
2017-05-29 09:49:47 +0300

    minor changes

--- Scanner/src/ScannerExample.java
+++ Scanner/src/ScannerExample.java
@@ -4,13 +4,21 @@ public class ScannerExample {
 
     public static void main(String[] args) {
 
+        int a, b, sum;
+
         Scanner s = new Scanner(System.in);
-        System.out.println("Введите число a:");
-        int a = s.nextInt();
-        System.out.println("Введите число b:");
-        int b = s.nextInt();
-        int sum = a + b;
-        System.out.println("Сумма: "+sum);
+
+        System.out.print("Enter a first number: ");
+
+        a = s.nextInt();
+
+        System.out.print("Enter a second number: ");
+
+        b = s.nextInt();
+
+        sum = a + b;
+
+        System.out.println("Sum is: "+sum);
     }
 }
 
