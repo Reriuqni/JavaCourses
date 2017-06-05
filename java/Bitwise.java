9151e5197a07ea2a2df2d3cc12c23656cbc8d986
nixon <nixon.white@gmail.com>
2017-06-05 11:10:06 +0300

    Messing with bitwise operations

---
+++ java/Bitwise.java
@@ -0,0 +1,26 @@
+public class Bitwise {
+
+	public static void main(String[] args) {
+
+		int a = 0b1111_1111_1111_1111_1111_1111_1111_1111;
+
+		int b = 0b0111_1111_1111_1111_1111_1111_1111_1111;	
+
+		System.out.println("a="+a);
+
+		System.out.println("a>>1="+(a>>1));
+
+		System.out.println("a<<1="+(a<<1));
+
+		System.out.println("a>>>1="+(a>>>1));
+
+		System.out.println("b="+b);
+
+		System.out.println("b>>1="+(b>>1));
+
+		System.out.println("b<<1="+(b<<1));
+
+		System.out.println("b>>>1="+(b>>>1));
+
+	}
+}
