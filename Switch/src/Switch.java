95857d687c4ae88bac443a7c5ef01d341e7e215b
nixon <nixon.white@gmail.com>
2017-06-08 22:54:13 +0300

    Messing with switch

--- Switch/src/Switch.java
+++ Switch/src/Switch.java
@@ -117,25 +117,25 @@ public class Switch {
 
         System.out.print("Введите номер месяца: ");
         a = s.nextInt();
-        int amountofdays;
+        int amountofdays = 0;
         if (a >= 1 && a <= 12) {
             switch (a) {
                 case 2:
                     amountofdays = 28;
                     break;
                 case 4:
-                    amountofdays = 30;
-                    break;
                 case 6:
-                    amountofdays = 30;
-                    break;
                 case 9:
-                    amountofdays = 30;
-                    break;
                 case 11:
                     amountofdays = 30;
                     break;
-                default:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
                     amountofdays = 31;
                     break;
             }
@@ -222,6 +222,134 @@ public class Switch {
         }
         System.out.println("--------------------------");
 
+        // 7
+        System.out.println("7. Единицы массы пронумерованы следующим образом: 1 — килограмм,\n" +
+                "2 — миллиграмм, 3 — грамм, 4 — тонна, 5 — центнер. Дан номер единицы\n" +
+                "массы (целое число в диапазоне 1–5) и масса тела в этих единицах (веще-\n" +
+                "ственное число). Найти массу тела в килограммах.");
 
+        System.out.print("Введите номер единицы массы: ");
+        a = s.nextInt();
+
+        System.out.print("Введите массу: ");
+        double mass = s.nextDouble();
+
+        double masskg;
+        k = 0;
+
+        if (a > 0 && a < 6) {
+            switch (a) {
+                case 1:
+                    k = 1;
+                    break;
+                case 2:
+                    k = 0.000001;
+                    break;
+                case 3:
+                    k = 0.001;
+                    break;
+                case 4:
+                    k = 1000;
+                    break;
+                case 5:
+                    k = 100;
+                    break;
+            }
+            masskg = k * mass;
+            System.out.println("Масса в кг: " + masskg);
+        } else {
+            System.out.println("Ошибочная операция!");
+        }
+        System.out.println("--------------------------");
+
+        // 8
+        System.out.println("8. Даны два целых числа: D (день) и M (месяц), определяющие правиль-\n" +
+                "ную дату невисокосного года. Вывести значения D и M для даты, предше-\n" +
+                "ствующей указанной.");
+
+        System.out.print("Введите день: ");
+        int d = s.nextInt();
+
+        System.out.print("Введите номер месяца: ");
+        int m = s.nextInt();
+
+        if (d > 0 && d < 32 && m > 0 && m < 13) {
+            int days = 0;
+            if (d == 1) {
+                if (--m < 1) m = 12;
+                switch (m) {
+                    case 2:
+                        days = 28;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        days = 30;
+                        break;
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        days = 31;
+                        break;
+                }
+                d = days;
+            } else {
+                d--;
+            }
+            System.out.println("Пред. дата: " + d + "-" + m);
+        } else {
+            System.out.println("Некорректная дата!");
+        }
+        System.out.println("--------------------------");
+
+        // 9
+        System.out.println("9. Даны два целых числа: D (день) и M (месяц), определяющие правиль-\n" +
+                "ную дату невисокосного года. Вывести значения D и M для даты, следую-\n" +
+                "щей за указанной.");
+
+        System.out.print("Введите день: ");
+        d = s.nextInt();
+
+        System.out.print("Введите номер месяца: ");
+        m = s.nextInt();
+
+        if (d > 0 && d < 32 && m > 0 && m < 13) {
+            int days = 0;
+            switch (m) {
+                case 2:
+                    days = 28;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    days = 31;
+                    break;
+            }
+            if (++d > days) {
+                d = 1;
+                if (++m > 12) m = 1;
+            }
+            System.out.println("След. дата: " + d + "-" + m);
+        } else {
+            System.out.println("Некорректная дата!");
+        }
+        System.out.println("--------------------------");
+
+        //TODO
     }
 }
