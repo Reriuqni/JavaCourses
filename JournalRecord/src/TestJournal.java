8fbd36dffd0f1baac4ec08c61653e9ecf16f24ff
nixon <nixon.white@gmail.com>
2017-08-12 09:25:18 +0300

    Syntax fix

--- JournalRecord/src/TestJournal.java
+++ JournalRecord/src/TestJournal.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public class TestJournal {
 
-    public static void main(String[] args) {git
+    public static void main(String[] args) {
 
         Date time = new Date();
         time.setTime(6747473);
